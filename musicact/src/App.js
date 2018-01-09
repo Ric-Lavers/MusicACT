9499c9e68270deab39fb04a0a5267b79bc72ac84
takuya <takuyaokamoto11@gmail.com>
2018-01-09 13:50:08 +1100

    initial commit

--- musicact/src/App.js
+++ musicact/src/App.js
@@ -3,20 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-//
-//   if (env.REACT_APP_SECRET_CODE) {
-//   console.log(env.REACT_APP_SECRET_CODE);
-// }
+  //
+  //   if (env.REACT_APP_SECRET_CODE) {
+  //   console.log(env.REACT_APP_SECRET_CODE);
+  // }
 
   render() {
-  return (
-    <div>
-      <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
-      <h2>{process.env.REACT_APP_SECRET_CODE}</h2>
-
-
-    </div>
-    )
+    return (
+      <div>
+        <small>
+          You are running this application in <b>{process.env.NODE_ENV}</b>
+          mode.
+        </small>
+        <h2>{process.env.REACT_APP_SECRET_CODE}</h2>
+      </div>
+    );
   }
 }
 
