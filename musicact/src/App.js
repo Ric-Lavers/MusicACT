02fdbf0df1bef4ea0e255cd726c5d39d718a413b
Ric-Lavers <ric_lavers@outlook.com>
2018-01-09 13:36:50 +1100

    inital commit

---
+++ musicact/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+//
+//   if (env.REACT_APP_SECRET_CODE) {
+//   console.log(env.REACT_APP_SECRET_CODE);
+// }
+
+  render() {
+  return (
+    <div>
+      <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
+      <h2>{process.env.REACT_APP_SECRET_CODE}</h2>
+
+
+    </div>
+    )
+  }
+}
+
+export default App;
