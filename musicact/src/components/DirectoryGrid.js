81f439fc40150a163f0020fc3a3719e2ec218624
Ric-Lavers <ric_lavers@outlook.com>
2018-01-12 19:32:57 +1100

    fixed some flex box issues

--- musicact/src/components/DirectoryGrid.js
+++ musicact/src/components/DirectoryGrid.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import image from '../images/second_sun.jpg';
+
 
 export const DirectoryGrid = ({listing}) => {
 
-  let backgroundImage = {backgroundImage:"src(../images/second_sun.jpg)"}
+  let backgroundImage = { backgroundImage:{image} }
   const divs = []
   for (let i = 0; i < listing.length; i++) {
     divs.push(
-      <div className="testing" style={backgroundImage}>
+      <div className="testing" >
 
-        <div className="inside-testing"> <h1> {`${listing[i]}`} </h1></div>
+        <div className="inside-testing">
+          <h1> {`${listing[i]}`} </h1>
+        </div>
       </div> )
   }
   console.log(divs);
