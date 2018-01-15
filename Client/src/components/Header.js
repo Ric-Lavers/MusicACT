b983e1451476c0123a8d871cc150e4149aaac3c6
takuya <takuyaokamoto11@gmail.com>
2018-01-15 11:25:54 +1100

    add basic authentication

--- Client/src/components/Header.js
+++ Client/src/components/Header.js
@@ -9,6 +9,12 @@ const Header = () => {
       <div>
         <nav>
           <ul>
+            <Link to={`/about`}> About </Link>
+            <Link to={`/news`}> News </Link>
+            <Link to={`/musicians`}> Musicians </Link>
+            <Link to={`/venues`}> Venues </Link>
+            <Link to={`/businesses`}> Businesses </Link>
+            <Link to={`/contact`}> Contact </Link>
             <Link to={`/signup`}> SignUp </Link>
             <Link to={`/login`}> LogIn </Link>
           </ul>
