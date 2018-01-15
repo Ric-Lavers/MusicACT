5682187c29ea54004b1f171b20c22db3e7b850ee
Richard <r.santos92@live.com>
2018-01-15 16:59:54 +1100

    added more footer images and fixed links

--- Client/src/components/Footer/Footer.js
+++ Client/src/components/Footer/Footer.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import classes from "./Footer.css"
 
+const apra = require("../../images/logo-apra.png");
+const canbragov = require("../../images/cbrcanbra.jpg");
+const nswgov = require("../../images/australian-government.png");
+const actgov = require("../../images/actgov.png");
+
 const Footer = () => {
   return (
     <div>
@@ -8,9 +13,9 @@ const Footer = () => {
             <nav>
                 <ul>
                 <a href="http://apraamcos.com.au/"><img src="http://www.musicnsw.com/wp-content/themes/musicnsw/images/logo-apra.png" /></a>
-                <a href="https://canberra.com.au/"><img src="http://www.musicnsw.com/wp-content/themes/musicnsw/images/logo-apra.png" /></a>
-                <a href="https://www.australia.gov.au/"><img src="http://www.musicnsw.com/wp-content/themes/musicnsw/images/logo-apra.png" /></a>
-                <a href="https://www.act.gov.au/"><img src="http://www.musicnsw.com/wp-content/themes/musicnsw/images/logo-apra.png" /></a>
+                <a href="https://canberra.com.au/"><img src={canbragov}/></a>
+                <a href="https://www.australia.gov.au/"><img src={nswgov} /></a>
+                <a href="https://www.act.gov.au/"><img src={actgov} /></a>
                 </ul>
             </nav>
         </div>
