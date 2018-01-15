70c6ab085643d58e694e88acb868fc295defee93
Richard <r.santos92@live.com>
2018-01-15 16:17:36 +1100

    add footer

---
+++ Client/src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import classes from "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div>
+      <div classes="footer">
+            <nav>
+                <ul>
+                <a href="http://apraamcos.com.au/"><img src="http://www.musicnsw.com/wp-content/themes/musicnsw/images/logo-apra.png" /></a>
+                <a href="https://canberra.com.au/"><img src="http://www.musicnsw.com/wp-content/themes/musicnsw/images/logo-apra.png" /></a>
+                <a href="https://www.australia.gov.au/"><img src="http://www.musicnsw.com/wp-content/themes/musicnsw/images/logo-apra.png" /></a>
+                <a href="https://www.act.gov.au/"><img src="http://www.musicnsw.com/wp-content/themes/musicnsw/images/logo-apra.png" /></a>
+                </ul>
+            </nav>
+        </div>
+    </div>
+  );
+};
+
+export default Footer;
